f98123e24d32fbe222cd8c82e7495a0c18ac0a01
slekup <74949101+slekup@users.noreply.github.com>
2023-07-02 13:40:51 +1200

    don't ignore folder case

---
+++ src/builders/value/index.ts
@@ -0,0 +1,6 @@
+export { default as ArrayValue } from './ArrayValue';
+export { default as BooleanValue } from './BooleanValue';
+export { default as IntegerValue } from './IntegerValue';
+export { default as NumberValue } from './NumberValue';
+export { default as ObjectValue } from './ObjectValue';
+export { default as StringValue } from './StringValue';
